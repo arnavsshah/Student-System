30d1d23d388300eb21cf88c4ab05230ac67b651f
arnav git <shah.arnav@yahoo.com>
2020-12-09 23:47:32 +0530

    library backend done

--- routes/library.js
+++ routes/library.js
@@ -5,23 +5,29 @@ var neo4jApi = require('../database/neo4jAPI/neo4jLibrary');
 
 
 router.get('/', async(req, res) => {
-    var interestBasedBooks = await neo4jApi.interestBased();
-    var studentBasedBook = await neo4jApi.bookBased();
-    var categoryStudentBasedBook = await neo4jApi.categoryBased();
-    var authorStudentBasedBook = await neo4jApi.authorBased();
-    res.send('hello');
+    var interestBased = await neo4jApi.interestBased(req);
+    var bookBased = await neo4jApi.bookBased(req);
+    var categoryBased = await neo4jApi.categoryBased(req);
+    var authorBased = await neo4jApi.authorBased(req);
+    var books = {
+        interestBased: interestBased,
+        bookBased: bookBased,
+        categoryBased: categoryBased,
+        authorBased: authorBased
+    }
+    res.send(books);
 })
 
 router.post('/issue/:id', async(req, res) => {
     var book_id = req.params.id;
-    var issueBook = await neo4jApi.issueBook(book_id);
-    res.send('hello');
+    var issueBook = await neo4jApi.issueBook(book_id, req);
+    res.send(issueBook);
 })
 
 router.post('/return/:id', async(req, res) => {
     var book_id = req.params.id;
-    var returnBook = await neo4jApi.returnBook(book_id);
-    res.send('hello');
+    var returnBook = await neo4jApi.returnBook(book_id, req);
+    res.send(returnBook);
 })
 
 
