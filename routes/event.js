116b42b45eb1ba137b7d40e38f353f0500d092e1
arnav git <shah.arnav@yahoo.com>
2020-12-12 17:16:37 +0530

    added file storage for events in backend

--- routes/event.js
+++ routes/event.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const fs = require('fs');
+const path = require("path");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: "../client/src",
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({
+    storage: storage,
+}).single('myImage')
 
 var neo4jApi = require('../database/neo4jAPI/neo4jEvent');
+
+
 
 
 router.get('/', async(req, res) => {
@@ -9,16 +25,6 @@ router.get('/', async(req, res) => {
     res.send(events);
 })
 
-router.post('/register', async(req, res) => {
-    // console.log('get post request')
-    var event_id = req.body.event_id;
-    await neo4jApi.registerForEvent(req.user.id, event_id);
-    res.send("registered for event");
-})
-router.post('/', async(req, res) => {
-    await neo4jApi.addEvent(req.body, req.user.id);
-    res.send("added event");
-})
 router.get('/registered', async(req, res) => {
     // console.log('in registered route')
     var events = await neo4jApi.getRegisteredEvents(req.user.id);
@@ -31,13 +37,32 @@ router.get('/:event_name', async(req, res) => {
     res.send(events);
 })
 
-
-
 router.get('/registered/:event_name', async(req, res) => {
     var event_name = req.params.event_name;
     var events = await neo4jApi.searchRegisteredEvents(event_name, req.user.id);
     res.send(events)
 });
+
+router.post('/', async(req, res) => {
+    upload(req, res, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            await neo4jApi.addEvent(req.body, req.user.id, req.file.filename);
+        }
+    })
+
+    res.send("added event");
+})
+
+router.post('/register', async(req, res) => {
+    // console.log('get post request')
+    var event_id = req.body.event_id;
+    await neo4jApi.registerForEvent(req.user.id, event_id);
+    res.send("registered for event");
+})
+
+
 
 // router.get('/:id', async(req, res) => {
 //     var event_id = req.params.id;
